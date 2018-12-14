a65052d29ccda984733eb287e2eb7bbafef762e2
howardreith <howard.reith@gmail.com>
2018-12-14 10:49:23 -0500

    Add validation for age and relationship
    -Add validation and alert messages for age and relationship

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 'use strict'
 
 const theForm = document.getElementsByTagName("form")[0]
+const theDiv = document.getElementsByClassName("builder")[0]
+const alert = document.createElement("h2")
+const ageInput = document.getElementsByTagName("input")[0]
+const relationshipInput = document.getElementsByTagName("select")[0]
+theDiv.appendChild(alert)
+
+console.log(relationshipInput)
 
 theForm.addEventListener("submit", (event) => {
   event.preventDefault()
+  if(+ageInput.value <= 0) {
+      alert.innerHTML = "Please enter an age greater than 0."
+  } else {
+      alert.innerHTML = ''
+      if (!relationshipInput.value) {
+        alert.innerHTML = "Please select a relationship status."
+      } else {
+        alert.innerHTML = ''
+  }
+  console.log(relationshipInput.value)
   console.log('Form Submitted')
+
+}
 })
