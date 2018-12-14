1b4968dddd853ec754bd602f8124e4ec6eaf0326
howardreith <howard.reith@gmail.com>
2018-12-14 11:45:35 -0500

    Add initial output div
    -Add an output div that displays the added information when the add
    button is clicked
    -Add DOM modification to require age input to be a number

--- index.js
+++ index.js
@@ -2,26 +2,44 @@
 
 const theForm = document.getElementsByTagName("form")[0]
 const theDiv = document.getElementsByClassName("builder")[0]
-const alert = document.createElement("h2")
 const ageInput = document.getElementsByTagName("input")[0]
+ageInput.type = "number" // Requires user to input a number
 const relationshipInput = document.getElementsByTagName("select")[0]
+const smokerInput = document.getElementsByName("smoker")[0]
 const addButton = document.getElementsByClassName("add")[0]
-addButton.type = 'button'
+addButton.type = 'button' // prevents add button from submitting form.
+const alert = document.createElement("h2")
+const outputDiv = document.createElement("div")
 theDiv.appendChild(alert)
+theForm.appendChild(outputDiv)
 
-console.log(relationshipInput)
-
-theForm.addEventListener("submit", (event) => {
-  event.preventDefault()
+addButton.addEventListener("click", (event) => {
+  console.log('Add Button Clicked')
   if(+ageInput.value <= 0) {
-      alert.innerHTML = "Please enter an age greater than 0."
+      alert.innerHTML = "Please enter an age that is greater than 0."
       return false
   } else if (!relationshipInput.value) {
       alert.innerHTML = "Please select a relationship status."
       return false
   } else {
+      console.log(typeof(+ageInput.value))
       alert.innerHTML = ''
+      let newPersonDiv = document.createElement("div")
+      newPersonDiv.setAttribute("style", "border-width: 5px;")
+      let outputAge = document.createElement("p")
+      let outputRelationship = document.createElement("p")
+      let outputSmoker = document.createElement("p")
+      outputAge.innerHTML = "Age: " + ageInput.value
+      outputRelationship.innerHTML = "Relationship: " + relationshipInput.value
+      outputSmoker.innerHTML = "Smoker? " + smokerInput.value
+      outputDiv.appendChild(newPersonDiv)
+      newPersonDiv.appendChild(outputRelationship)
+      newPersonDiv.appendChild(outputAge)
+      newPersonDiv.appendChild(outputSmoker)
   }
-  console.log(relationshipInput.value)
+})
+
+theForm.addEventListener("submit", (event) => {
+  event.preventDefault()
   console.log('Form Submitted')
 })
