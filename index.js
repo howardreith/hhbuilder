c8d8f8748d7ad2191071c00999e0b905f1461d16
howardreith <howard.reith@gmail.com>
2018-12-14 10:56:41 -0500

    Fix validation
    -Validation was still submitting form in some incorrect conditions.
    -Fixed

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const theDiv = document.getElementsByClassName("builder")[0]
 const alert = document.createElement("h2")
 const ageInput = document.getElementsByTagName("input")[0]
 const relationshipInput = document.getElementsByTagName("select")[0]
+const addButton = document.getElementsByClassName("add")[0]
+addButton.type = 'button'
 theDiv.appendChild(alert)
 
 console.log(relationshipInput)
@@ -13,15 +15,13 @@ theForm.addEventListener("submit", (event) => {
   event.preventDefault()
   if(+ageInput.value <= 0) {
       alert.innerHTML = "Please enter an age greater than 0."
+      return false
+  } else if (!relationshipInput.value) {
+      alert.innerHTML = "Please select a relationship status."
+      return false
   } else {
       alert.innerHTML = ''
-      if (!relationshipInput.value) {
-        alert.innerHTML = "Please select a relationship status."
-      } else {
-        alert.innerHTML = ''
   }
   console.log(relationshipInput.value)
   console.log('Form Submitted')
-
-}
 })
